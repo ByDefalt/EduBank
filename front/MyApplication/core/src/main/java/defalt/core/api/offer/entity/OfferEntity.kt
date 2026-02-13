32851288aa9b6c5552f24438b9d9b293b3f468f4
ByDefalt <rousvalromain@gmail.com>
2026-02-13 23:04:45 +0100

    Update OfferEntity.kt

--- front/MyApplication/core/src/main/java/defalt/core/api/offer/entity/OfferEntity.kt
+++ front/MyApplication/core/src/main/java/defalt/core/api/offer/entity/OfferEntity.kt
@@ -1,20 +1,18 @@
 package defalt.core.api.offer.entity
 
-import kotlinx.serialization.SerialName
-import kotlinx.serialization.Serializable
 import java.time.LocalDate
 
 data class OfferEntity(
     val id: Int,
     val title: String,
     val description: String,
-    val state: OfferEntity.State,
+    val state: State,
     val startDate: LocalDate,
     val endDate: LocalDate,
     val picturePath: String? = null,
 
     ) {
-    enum class State(val value: kotlin.String) {
+    enum class State(val value: String) {
         ACTIVE("active"),
         INACTIVE("inactive"),
         EXPIRED("expired"),
