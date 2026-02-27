277613ebfb88f2cc9a4edc2690f776d4f773600c
ByDefalt <rousvalromain@gmail.com>
2026-02-27 22:16:50 +0100

    Remove KSP and Kotlin Serialization plugins
    Remove the KSP and Kotlin Serialization plugin aliases from app/build.gradle.kts. Delete the Kotlin compiler session file (.kotlin/sessions/*.salive) and tidy up a stray blank line in the dependencies block.

--- front/MyApplication/app/build.gradle.kts
+++ front/MyApplication/app/build.gradle.kts
@@ -4,8 +4,6 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.ksp)
-    alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.spotless)
 }
 
@@ -49,7 +47,6 @@ android {
 }
 
 dependencies {
-
     implementation(project(":core:network"))
     implementation(project(":core:database"))
     implementation(project(":core:ui"))
