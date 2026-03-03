e9cded7b04d66ead6790fa71b2533a1647fc6435
elebras1 <erwan.lebras27@gmail.com>
2026-03-03 11:35:59 +0100

    feat : filter d'authentification + handler exception + lien avec accountAPi via Feign

--- back/OperationAPI/src/main/java/com/operationapi/configuration/JerseyConfig.java
+++ back/OperationAPI/src/main/java/com/operationapi/configuration/JerseyConfig.java
@@ -1,5 +1,8 @@
 package com.operationapi.configuration;
 
+import com.operationapi.filters.AuthenticationFilter;
+import com.operationapi.handlers.FunctionalExceptionHandler;
+import com.operationapi.handlers.NotFoundExceptionHandler;
 import jakarta.ws.rs.ApplicationPath;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.context.annotation.Configuration;
@@ -7,7 +10,10 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ApplicationPath("/api/v1")
 public class JerseyConfig extends ResourceConfig {
-  public JerseyConfig() {
-    packages("com.operationapi.controller");
-  }
+    public JerseyConfig() {
+        packages("com.operationapi.controller");
+        register(AuthenticationFilter.class);
+        register(FunctionalExceptionHandler.class);
+        register(NotFoundExceptionHandler.class);
+    }
 }
