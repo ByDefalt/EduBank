eb0885b7196d3e271bb8f15af09705c442f9af42
ByDefalt <rousvalromain@gmail.com>
2026-03-05 13:43:24 +0100

    Add account remote method stubs and imports
    Update AccountRemoteDataSource: add imports for SignIn/Token, Role and PersonalInformation entities and introduce multiple new stubbed remote methods (activateAccount, deactivateAccount, signIn, validateToken, role and account-role queries, personal information CRUD queries). Also change deleteAccount signature to use String id and return NetworkResult<Boolean>. Methods remain TODO implemented.

--- front/MyApplication/core/network/src/main/java/defalt/network/datasource/account/AccountRemoteDataSource.kt
+++ front/MyApplication/core/network/src/main/java/defalt/network/datasource/account/AccountRemoteDataSource.kt
@@ -3,6 +3,12 @@ package defalt.network.datasource.account
 import defalt.domain.datasource.account.IAccountRemoteDataSource
 import defalt.domain.entity.account.Account
 import defalt.domain.entity.account.AccountRegister
+import defalt.domain.entity.account.PersonalInformation
+import defalt.domain.entity.account.PersonalInformationRegister
+import defalt.domain.entity.account.Role
+import defalt.domain.entity.account.SignInRequest
+import defalt.domain.entity.account.TokenRequest
+import defalt.domain.entity.account.TokenResponse
 import defalt.network.api.account.service.AccountApi
 import defalt.utils.NetworkResult
 
@@ -21,7 +27,52 @@ class AccountRemoteDataSource(
         TODO("Not yet implemented")
     }
 
-    override suspend fun deleteAccount(id: Int): NetworkResult<Unit> {
+    override suspend fun deleteAccount(id: String): NetworkResult<Boolean> {
         TODO("Not yet implemented")
     }
+
+    override suspend fun activateAccount(id: String): NetworkResult<Boolean> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deactivateAccount(id: String): NetworkResult<Boolean> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun signIn(signInRequest: SignInRequest): NetworkResult<TokenRequest> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun validateToken(tokenRequest: TokenRequest): NetworkResult<TokenResponse> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getRoles(): NetworkResult<List<Role>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getRoleById(id: Int): NetworkResult<Role> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getAccountRole(accountId: String): NetworkResult<Role> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getPersonalInformations(): NetworkResult<List<PersonalInformation>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getPersonalInformationById(id: Int): NetworkResult<PersonalInformation> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getPersonalInformationByAccountId(accountId: String): NetworkResult<PersonalInformation> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun createPersonalInformation(personalInformationRegister: PersonalInformationRegister): NetworkResult<PersonalInformation> {
+        TODO("Not yet implemented")
+    }
+
 }
