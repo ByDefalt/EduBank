1b87d79afcc58b3d5924e4f544a752753725b535
ByDefalt <rousvalromain@gmail.com>
2026-03-05 13:23:43 +0100

    Expand account remote datasource API
    Add new account-related endpoints and scaffolding. IAccountRemoteDataSource gained methods for account activation/deactivation, authentication (signIn/validateToken), role management, and personal information CRUD; deleteAccount signature changed from (Int) -> Unit to (String) -> Boolean. AccountRemoteDataSource constructor renamed the AccountApi param and now accepts PersonalInformationApi and RoleApi; stub implementations (TODOs) and necessary imports were added to match the updated interface.

--- front/MyApplication/core/domain/src/main/java/defalt/domain/datasource/account/IAccountRemoteDataSource.kt
+++ front/MyApplication/core/domain/src/main/java/defalt/domain/datasource/account/IAccountRemoteDataSource.kt
@@ -2,11 +2,35 @@ package defalt.domain.datasource.account
 
 import defalt.domain.entity.account.Account
 import defalt.domain.entity.account.AccountRegister
+import defalt.domain.entity.account.PersonalInformation
+import defalt.domain.entity.account.PersonalInformationRegister
+import defalt.domain.entity.account.Role
+import defalt.domain.entity.account.SignInRequest
+import defalt.domain.entity.account.TokenRequest
+import defalt.domain.entity.account.TokenResponse
 import defalt.utils.NetworkResult
 
 interface IAccountRemoteDataSource {
+    // --- COMPTES ---
     suspend fun getAccounts(): NetworkResult<List<Account>>
     suspend fun getAccountById(id: String): NetworkResult<Account>
     suspend fun createAccount(accountRegister: AccountRegister): NetworkResult<Account>
-    suspend fun deleteAccount(id: Int): NetworkResult<Unit>
+    suspend fun deleteAccount(id: String): NetworkResult<Boolean>
+    suspend fun activateAccount(id: String): NetworkResult<Boolean>
+    suspend fun deactivateAccount(id: String): NetworkResult<Boolean>
+
+    // --- AUTHENTIFICATION ---
+    suspend fun signIn(signInRequest: SignInRequest): NetworkResult<TokenRequest>
+    suspend fun validateToken(tokenRequest: TokenRequest): NetworkResult<TokenResponse>
+
+    // --- RÔLES ---
+    suspend fun getRoles(): NetworkResult<List<Role>>
+    suspend fun getRoleById(id: Int): NetworkResult<Role>
+    suspend fun getAccountRole(accountId: String): NetworkResult<Role>
+
+    // --- INFORMATIONS PERSONNELLES ---
+    suspend fun getPersonalInformations(): NetworkResult<List<PersonalInformation>>
+    suspend fun getPersonalInformationById(id: Int): NetworkResult<PersonalInformation>
+    suspend fun getPersonalInformationByAccountId(accountId: String): NetworkResult<PersonalInformation>
+    suspend fun createPersonalInformation(personalInformationRegister: PersonalInformationRegister): NetworkResult<PersonalInformation>
 }
